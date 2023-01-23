f6a41c8e7c6098a81f7e1f38cfc0a6fe84216c96
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-01-24 00:07:10 +0800

    added function to render license link in generateMarkdown js file, it's been tested and it's working

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -23,6 +23,29 @@ function renderLicenseBadge(license) {
 // // If there is no license, return an empty string
 // function renderLicenseLink(license) {}
 
+function renderLicenseLink(license) {
+  if (license === licenseArray[0]) {
+    return `[${licenseArray[0]}](https://opensource.org/licenses/Apache-2.0)`
+
+  } else if (license === licenseArray[1]) {
+    return `[${licenseArray[1]}](https://opensource.org/licenses/BSD-3-Clause)`
+
+  } else if (license === licenseArray[2]) {
+    return `[${licenseArray[2]}](https://opensource.org/licenses/MPL-2.0)`
+
+  } else if (license === licenseArray[3]) {
+    return `[${licenseArray[3]}](https://opensource.org/licenses/MS-PL)`
+
+  } else if (license === licenseArray[4]) {
+    return `[${licenseArray[4]}](https://opensource.org/licenses/MIT)`
+  
+  } else if (license === licenseArray[5]) {
+    return `[${licenseArray[5]}](https://opensource.org/licenses/OSL-3.0)`
+  } else {
+    return ""
+  } 
+}
+
 // // TODO: Create a function that returns the license section of README
 // // If there is no license, return an empty string
 // function renderLicenseSection(license) {}
@@ -30,7 +53,7 @@ function renderLicenseBadge(license) {
 
 
 //${renderLicenseSection(data.license)}
-//${renderLicenseLink(data.license)}
+
 
 
 
@@ -40,10 +63,7 @@ function generateMarkdown(data) {
   
   return `
   
-# ${data.title}  
-
-## Badges
-${renderLicenseBadge(data.license)} 
+# ${data.title}      ${renderLicenseBadge(data.license)} 
 
 ---------------------------------------------------------------
 
@@ -76,7 +96,7 @@ ${data.guidelines}
 
 
 ## License
-${data.license}
+${renderLicenseLink(data.license)}
 
 
 
